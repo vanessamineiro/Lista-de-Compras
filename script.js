c4273342c3cb52f9f21b391ebd2c70c3cac96125
MVanessa <mj.vanessa@hotmail.com>
2026-02-17 19:54:58 -0300

    Inclusão de item

---
+++ script.js
@@ -0,0 +1,44 @@
+const form = document.querySelector("form")
+const list = document.querySelector("ul")
+const item = document.getElementById("new-item")
+
+try {
+  form.onsubmit = (event) => {
+    event.preventDefault()
+
+    if (!item.value) {
+      alert("Preencha o nome do item")
+    } else {
+
+      const newItem = document.createElement("li")
+
+      const newCheckbox = document.createElement("input")
+      newCheckbox.setAttribute("type", "checkbox")
+      newCheckbox.setAttribute("name", "item")
+
+      const newLabel = document.createElement("label")
+      newLabel.setAttribute("for", "item")
+      newLabel.textContent = item.value
+
+      const newButton = document.createElement("button")
+
+      const trash = document.createElement("img")
+      trash.setAttribute("src", "assets/trash.svg")
+      trash.setAttribute("alt", "Remover item")
+
+      newButton.append(trash)
+
+      newItem.append(newCheckbox, newLabel, newButton)
+      list.append(newItem)
+
+      item.value = ""
+      item.focus()
+    }
+  }
+
+} catch (error) {
+  console.log(error)
+  alert("Não foi possível adicionar o item")
+
+}
+
